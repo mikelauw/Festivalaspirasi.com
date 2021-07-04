cbc6756b7077962be24c3ef0067053cbdd014b0e
mikelauw <michaelmike162@gmail.com>
2021-07-04 13:44:53 +0700

    make route api

--- domains/festivalaspirasi.com/public_html/routes/api.php
+++ domains/festivalaspirasi.com/public_html/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/list-tema/{ctg}', 'WebController@showTema');
+Route::get('/list-questioner/{tema}', 'WebController@showQuestioner');
